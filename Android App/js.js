340c244f54b668f88f4c3d4eb6fb44d639345db1
rubyproducti9n <91490800+rubyproducti9n@users.noreply.github.com>
2021-10-13 21:18:47 +0530

    Add files via upload

---
+++ Android App/js.js
@@ -0,0 +1,29 @@
+var div = document.getElementById('notification');
+div.innerHTML += ' <a class="nav-link navbar-icon" href="notifications.html" aria-expanded="false">\
+<i class="fas fa-bell"></i>\
+<span class="badge rounded-pill badge-notification bg-danger">1</span>\
+</a>';
+
+// -----------------------------------------------------------
+// Rrandomization Function Below
+var random = Math.floor(Math.random() * $('.random').length);
+$('.random').hide().eq(random).show();
+
+
+// const chk = document.getElementById('chk');
+
+// chk.addEventListener('click', () => {
+//   chk.checked?document.body.classList.add("dark"):document.body.classList.remove("dark");
+//   localStorage.setItem('darkModeStatus', chk.checked);
+// });
+
+// window.addEventListener('load', (event) => {
+//   if(localStorage.getItem('darkModeStatus')=="true"){
+//     document.body.classList.add("dark");
+//     document.getElementById('chk').checked = true;
+//   }
+// });
+
+$(window).load(function() {
+  $('#loading').hide();
+});
